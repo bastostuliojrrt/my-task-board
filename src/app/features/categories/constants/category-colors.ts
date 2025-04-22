b76e9b0c57490c506404144ffbc02207d6b42822
raydevcp20 <rayanecristina.19@gmail.com>
2025-04-22 17:58:02 -0300

    Feat: Componentization of categories

---
+++ src/app/features/categories/constants/category-colors.ts
@@ -0,0 +1,8 @@
+export const categoryColorsBackground: Record<string, string> = {
+    green: 'bg-green-600',
+    orange: 'bg-orange-600',
+    red: 'bg-red-600',
+    blue: 'bg-blue-600',
+    purple: 'bg-purple-600',
+    gray: 'bg-gray-600',
+}
--- src/app/features/categories/view/category/category.component.ts
+++ src/app/features/categories/view/category/category.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MainListComponent } from '../../components/main-list/main-list.component';
+import { ColorsListComponent } from '../../components/colors-list/colors-list.component';
+import { CategoryService } from '../../services/category.service';
+import { AsyncPipe } from '@angular/common';
+
+const COMPONENTS = [MainListComponent,ColorsListComponent];
+
+const PIPES = [AsyncPipe];
 
 @Component({
   selector: 'app-category',
   standalone: true,
-  imports: [],
+  imports: [...COMPONENTS, ...PIPES],
   templateUrl: './category.component.html',
   styles: ''
 })
 export class CategoryComponent {
+
+    // Injetando a dependência CategoryService, que será utilizada para fazer as requisições à API 
+    private readonly categoryService = inject(CategoryService);
+
+    // Chamando o método getCategories do CategoryService, que retorna um Observable<Category[]>, e armazenando o resultado na variável categories$
+    public categories$ = this.categoryService.getCategories();
 
 }
